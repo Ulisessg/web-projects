7621b9ed7887155f3d02c704448a2b2afea9f8ef
UlisesSG <ukisessg@gmail.com>
2020-09-27 01:36:06 -0500

    Change in the order of folders

--- src/pages/designSystem/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import '../../styles/common/default-styles.styl';
-
-import '../../styles/common/header.styl';
-
-ReactDom.render(<h1>Hello Design</h1>, document.getElementById('root'));
--- src/pages/main/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-import '../../styles/common/default-styles.styl';
-
-ReactDom.render(<h1>Hello World</h1>, document.getElementById('root'));
