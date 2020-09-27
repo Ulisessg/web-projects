c0cd6a23cd694d41550796a65c2ed11809348618
UlisesSG <ukisessg@gmail.com>
2020-09-27 03:06:17 -0500

    Variables.styl and Default-styles added

--- src/react/pages/blog-index.js
+++ src/react/pages/blog-index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import '../../styles/default-styles.styl';
+
 ReactDom.render(<h1>Hello Blog</h1>, document.getElementById('root'));
--- src/react/pages/main-index.js
+++ src/react/pages/main-index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import '../../styles/atoms/test.styl';
+import '../../styles/default-styles.styl';
 
 ReactDom.render(<h1>Hello Main</h1>, document.getElementById('root'));
