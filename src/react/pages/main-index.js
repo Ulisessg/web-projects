55e40cf3b5d456c05ae76152fbfc200041e8bfae
UlisesSG <ukisessg@gmail.com>
2020-10-06 02:12:27 -0500

    Basic seo and Favicon added at index

--- src/react/pages/main-index.js
+++ src/react/pages/main-index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+//Temporal imports 🤖
+import { Helmet } from 'react-helmet';
+import icon from '../../../public/favicon.ico';
 
 import '../../styles/default-styles.styl';
 
-ReactDom.render(<h1>Hello Main</h1>, document.getElementById('root'));
+ReactDom.render(
+  <>
+    <Helmet>
+      <title>Ulises Dev</title>
+      <meta
+        name='description'
+        content='Portafolio de @Ulises5G ulisessg Ulises Antonio Samano Galvan'
+      />
+      <link rel='icon' type='image/vnd.microsoft.icon' href={icon} />
+    </Helmet>
+    <h1>Hello Main</h1>
+  </>,
+  document.getElementById('root'),
+);
