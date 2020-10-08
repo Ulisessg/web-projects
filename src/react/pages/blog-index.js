9381eacc7dd49068680ff65f7f23860a15a9ef2a
UlisesSG <ukisessg@gmail.com>
2020-10-08 00:25:27 -0500

    Blog improvements

--- src/react/pages/blog-index.js
+++ src/react/pages/blog-index.js
@@ -1,37 +1,14 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import ReactDom from 'react-dom';
-import { Helmet } from 'react-helmet';
-
-import Icon from '../../images/favicon.png';
 
 //Styles
-import '../../styles/default-styles.styl';
-import '../../styles/pages/redirect.styl';
 
 //Components
 const Blog = lazy(() => import('../templates/Blog'));
 
 ReactDom.render(
   <>
-    <Suspense fallback={<h1>Loading...</h1>}>
-      <Helmet>
-        <link rel='icon' type='image/vnd.microsoft.icon' href={Icon} />
-        <meta name='description' content='Noticias de tecnologia' />
-      </Helmet>
-
-      <Blog />
-
-      <h1>
-        Work in progress...
-        <span role='img' aria-label='emoji'>
-          😀
-        </span>
-      </h1>
-      <br />
-      <a href='/design' className='link'>
-        <h1>Actualmente estoy trabajando en un sistema de diseño</h1>
-      </a>
-    </Suspense>
+    <Blog />
   </>,
   document.getElementById('root'),
 );
--- src/react/templates/Blog.jsx
+++ src/react/templates/Blog.jsx
@@ -1,5 +1,12 @@
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
+import { Helmet } from 'react-helmet';
+import Icon from '../../images/favicon.png';
 
+//Styles
+import '../../styles/default-styles.styl';
+import '../../styles/pages/redirect.styl';
+
+//Components
 const Header = lazy(() => import('../organisms/Header'));
 
 const paths = [
@@ -23,7 +30,24 @@ const paths = [
 const Blog = () => {
   return (
     <>
-      <Header paths={paths} />
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Helmet>
+          <link rel='icon' type='image/vnd.microsoft.icon' href={Icon} />
+          <meta name='description' content='Noticias de tecnologia' />
+        </Helmet>
+        <Header paths={paths} />
+
+        <h1>
+          Work in progress...
+          <span role='img' aria-label='emoji'>
+            😀
+          </span>
+        </h1>
+        <br />
+        <a href='/design' className='link'>
+          <h1>Actualmente estoy trabajando en un sistema de diseño</h1>
+        </a>
+      </Suspense>
     </>
   );
 };
