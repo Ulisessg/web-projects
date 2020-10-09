5731610912d36f28dd24e8cc9ceabbe858d5bc77
UlisesSG <ukisessg@gmail.com>
2020-10-09 01:57:11 -0500

    Error 404 improvements

--- src/react/templates/Error.jsx
+++ src/react/templates/Error.jsx
@@ -1,4 +1,6 @@
 import React, { lazy, Suspense } from 'react';
+import { Helmet } from 'react-helmet';
+import Icon from '../../images/favicon.png';
 import Loading from '../atoms/Loading';
 import { paths } from '../../localData.json';
 
@@ -10,6 +12,14 @@ const Error = () => {
   return (
     <>
       <Suspense fallback={<Loading />}>
+        <Helmet>
+          <title>Error 404</title>
+          <meta
+            name='description'
+            content='Sistema de diseño utilizado para construir mis productos'
+          />
+          <link rel='icon' type='image/vnd.microsoft.icon' href={Icon} />
+        </Helmet>
         <Header paths={paths} />
         <h1 className='message'>
           Página no encontrada o en proceso de construcción&nbsp;
