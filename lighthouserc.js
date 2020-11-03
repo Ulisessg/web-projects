e08820da0c0d6f2c7aa4df44bbffe000b2d522df
Ulisessg <ukisessg@gmail.com>
2020-11-02 19:23:02 -0600

    [ADD] Accessibility and good practices are 100%

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,8 +8,8 @@ module.exports = {
     assert: {
       assertions: {
         'categories:performance': ['error', { minScore: 0.95 }],
-        'categories:accessibility': ['error', { minScore: 0.95 }],
-        'categories:best-practices': ['error', { minScore: 0.99 }],
+        'categories:accessibility': ['error', { minScore: 1 }],
+        'categories:best-practices': ['error', { minScore: 1 }],
       },
     },
     upload: {
