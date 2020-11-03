5488ceb152c20651d0b8747b4ea5e4c7eaf755d3
Ulisessg <ukisessg@gmail.com>
2020-11-02 22:50:35 -0600

    [DELETED] Unused regenerator-runtime

--- src/react/pages/design-index.js
+++ src/react/pages/design-index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-//This line is mandatory
-// eslint-disable-next-line no-unused-vars
-import Regenerator from 'regenerator-runtime';
 //App
 import Design from '../templates/Design';
 
