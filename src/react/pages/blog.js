21941f49d624b4ca39be6a9be1da731c409c691e
Ulisessg <ukisessg@gmail.com>
2020-12-14 04:17:28 -0600

    [ADD] Store created from blog

--- src/react/pages/blog.js
+++ src/react/pages/blog.js
@@ -3,12 +3,11 @@ import React, { lazy, Suspense } from 'react';
 import ReactDom from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducers from '../reducers/initialReducer';
-import initialState from '../store/initialState';
 import Loading from '../atoms/Loading';
+import reducers from '../reducers/blog';
 //App
 const Blog = lazy(() => import('../templates/Blog'));
-const store = createStore(reducers, initialState);
+const store = createStore(reducers, {});
 ReactDom.render(
   <>
     <Suspense fallback={<Loading />}>
---
+++ src/react/reducers/blog/index.js
@@ -0,0 +1,7 @@
+//@ts-check
+import { combineReducers } from 'redux';
+import headerReducer from '../common/headerReducer';
+
+export default combineReducers({
+  headerReducer,
+});
