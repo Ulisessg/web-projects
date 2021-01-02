61b02a2edbaed864af20d985c3356d89d68c6e13
Ulisessg <ukisessg@gmail.com>
2021-01-02 07:00:05 -0600

    [FIX] SEO fixed

--- src/react/organisms/MainBlog.jsx
+++ src/react/organisms/MainBlog.jsx
@@ -1,18 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet';
-
 import '../../styles/organisms/mainBlog-styles.styl';
 
 const MainBlog = () => {
   const [blogContent, setBlogContent] = useState(null);
-  const [blogInfo, setBlogInfo] = useState(null);
 
   let blogRequested = false;
-  let blogInfoRequested = false;
 
   const query = window.location.search.split('=')[1];
   const urlBlog = `https://web-projects-api.ulisessg.vercel.app/api/blog?name=${query}`;
-  const urlBlogInfo = `https://web-projects-api.ulisessg.vercel.app/api/blog/info?name=${query}`;
 
   // Get blog
   useEffect(() => {
@@ -29,23 +24,6 @@ const MainBlog = () => {
         console.error(err);
       });
   }, [blogRequested]);
-
-  // Get blog info
-  useEffect(() => {
-    window
-      .fetch(urlBlogInfo)
-      .then((res) => {
-        blogInfoRequested = true;
-        return res.json();
-      })
-      .then((data) => {
-        setBlogInfo(data);
-        console.log(blogInfo);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, [blogInfoRequested]);
 
   const printBlog = () => {
     const PARSER = new DOMParser();
@@ -69,35 +47,11 @@ const MainBlog = () => {
 
   return (
     <>
-      {/* SEO */}
-      {blogInfo && (
-        <Helmet>
-          <meta name='description' content={blogInfo.message.metaDescription} />
-          <meta name='twitter:title' content={blogInfo.message.title} />
-          <meta
-            name='twitter:description'
-            content={blogInfo.message.metaDescription}
-          />
-          <meta name='twitter:image' content={blogInfo.message.seoCardUrl} />
-          <meta property='og:title' content={blogInfo.message.title} />
-          <meta
-            property='og:description'
-            content={blogInfo.message.metaDescription}
-          />
-          <meta property='og:image' content={blogInfo.message.seoCardUrl} />
-          <meta
-            property='og:url'
-            content={`https://ulisessg.com/blog${window.location}`}
-          />
-          <title>{blogInfo.message.title}</title>
-        </Helmet>
-      )}
-
       {/* Content */}
 
       <main id='main'>
         <section className='blog-wrapper' id='blogWrapper'>
-          {blogContent ? printBlog() : <h1>Loading blog...</h1>}
+          {blogContent ? printBlog() : null}
         </section>
       </main>
     </>
--- src/react/pages/blog.js
+++ src/react/pages/blog.js
@@ -1,7 +1,11 @@
 //@ts-check
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
 import ReactDom from 'react-dom';
 import { createStore } from 'redux';
+import { Helmet } from 'react-helmet';
+
+import 'regenerator-runtime';
+
 import { Provider } from 'react-redux';
 import reducers from '../reducers/blog';
 import Loading from '../atoms/Loading';
@@ -9,8 +13,59 @@ import Loading from '../atoms/Loading';
 //App
 const Blog = lazy(() => import('../templates/Blog'));
 const store = createStore(reducers, {});
+
+const SEO = () => {
+  const query = window.location.search.split('=')[1];
+  const urlBlogInfo = `https://web-projects-api.ulisessg.vercel.app/api/blog/info?name=${query}`;
+  const [blogInfo, setBlogInfo] = useState(null);
+  let infoRequested = false;
+
+  useEffect(() => {
+    window
+      .fetch(urlBlogInfo)
+      .then((res) => {
+        infoRequested = true;
+        return res.json();
+      })
+      .then((data) => {
+        setBlogInfo(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [infoRequested]);
+
+  return (
+    <>
+      {blogInfo && (
+        <Helmet>
+          <meta name='description' content={blogInfo.message.metaDescription} />
+          <meta name='twitter:title' content={blogInfo.message.title} />
+          <meta
+            name='twitter:description'
+            content={blogInfo.message.metaDescription}
+          />
+          <meta name='twitter:image' content={blogInfo.message.seoCardUrl} />
+          <meta property='og:title' content={blogInfo.message.title} />
+          <meta
+            property='og:description'
+            content={blogInfo.message.metaDescription}
+          />
+          <meta property='og:image' content={blogInfo.message.seoCardUrl} />
+          <meta
+            property='og:url'
+            content={`https://ulisessg.com/blog${window.location.search}`}
+          />
+          <title>{blogInfo.message.title}</title>
+        </Helmet>
+      )}
+    </>
+  );
+};
+
 ReactDom.render(
   <>
+    <SEO />
     <Suspense fallback={<Loading />}>
       <Provider store={store}>
         <Blog />
