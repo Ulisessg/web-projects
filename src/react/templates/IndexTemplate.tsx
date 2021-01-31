2435f5f4e167ec811b7de73b1fc0617750d58bbc
Ulisessg <ukisessg@gmail.com>
2021-01-31 05:43:13 -0600

    [ADD] Layout component added in index page

--- src/react/templates/IndexTemplate.tsx
+++ src/react/templates/IndexTemplate.tsx
@@ -3,25 +3,26 @@ import React from 'react';
 //  Styles
 import '../../styles/default-styles.styl';
 import '../../styles/templates/blog-styles.styl';
-import '../../styles/atoms/footer.styl';
-import '../../styles/organisms/header.styl';
-import '../../styles/atoms/skipLink.styl';
+
+import Layout from '../organisms/Layout';
 
 function Main(): JSX.Element {
   return (
     <>
-      <main>
-        <h1>
-          Work in progress...
-          <span role='img' aria-label='emoji'>
-            😄
-          </span>
-        </h1>
-        <br />
-        <a href='design.html' className='link'>
-          <h1>Actualmente estoy trabajando en un sistema de diseño</h1>
-        </a>
-      </main>
+      <Layout>
+        <main>
+          <h1>
+            Work in progress...
+            <span role='img' aria-label='emoji'>
+              😄
+            </span>
+          </h1>
+          <br />
+          <a href='design.html' className='link'>
+            <h1>Actualmente estoy trabajando en un sistema de diseño</h1>
+          </a>
+        </main>
+      </Layout>
     </>
   );
 }
