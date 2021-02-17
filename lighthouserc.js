7dbdd695798ccecc545323537301a323f5770e5a
Ulisessg <ukisessg@gmail.com>
2021-02-17 03:06:16 -0600

    [ADD] Addeed pages to performance analysis in github actions

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,29 +1,22 @@
 //  @ts-check
 
-const { readdirSync } = require('fs');
-const { join } = require('path');
-
-const directiories = readdirSync(join(__dirname, 'public'), {
-  encoding: 'utf-8',
-});
-
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: './dist',
-      url: directiories,
+      staticDistDir: "./dist",
+      url: ["index.html", "blog.html"],
       numberOfRuns: 5,
     },
     assert: {
       assertions: {
         //  Temporally downgraded
-        'categories:performance': ['error', { minScore: 0.8 }],
-        'categories:accessibility': ['error', { minScore: 0.9 }],
-        'categories:best-practices': ['error', { minScore: 0.9 }],
+        "categories:performance": ["error", { minScore: 0.8 }],
+        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:best-practices": ["error", { minScore: 0.9 }],
       },
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: "temporary-public-storage",
     },
   },
 };
