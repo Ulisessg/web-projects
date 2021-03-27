2ddcf72a6bb7d767bfc4f7ef63d999aede77c6d3
ulisessg <ukisessg@gmail.com>
2021-03-26 23:37:27 -0600

    Lazy image component finished

--- src/react/molecules/Img.tsx
+++ src/react/molecules/Img.tsx
@@ -2,7 +2,7 @@ import React, { lazy } from 'react';
 import { imgProps } from '../interfaces';
 
 const Element = lazy(() => {
-  if ('loading' in HTMLImageElement.prototype) {
+  if (navigator !== undefined && 'loading' in HTMLImageElement.prototype) {
     return import('../atoms/LazyImage');
   }
   return import('../atoms/LazyObserverImage');
