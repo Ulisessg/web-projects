dd2a43210dff236a48f046b3e2047e6d859e428c
ulisessg <ukisessg@gmail.com>
2021-04-02 16:20:00 -0600

    Footer updated to v2

--- src/react/molecules/Footer.tsx
+++ src/react/molecules/Footer.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import '../../styles/atoms/footer.styl';
+import '../../styles/molecules/footer.styl';
 import Nav from '../atoms/Nav';
 import LinkedInBadge from './LinkedInBadge';
+import LazyImage from './Img';
 
 interface Path {
   id: number;
@@ -15,34 +16,77 @@ function Footer(): JSX.Element {
   const footerPaths: Array<Path> = [
     {
       id: 1,
-      description: 'Mi Twitter',
-      name: 'Mi Twitter',
-      path: 'https://twitter.com/Ulises5G',
-    },
-    {
-      id: 2,
       description: 'Mi perfil de Platzi',
       name: 'Perfil de platzi',
       path: 'https://platzi.com/p/UlisesSG/',
     },
     {
-      id: 3,
-      description: 'Mi LinkedIn',
-      name: 'LinkedIn',
-      path: 'https://www.linkedin.com/in/ulisessg/',
+      id: 2,
+      description: 'Curriculum',
+      name: 'Curriculum',
+      path:
+        'https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2FFullstack%20React%20Developer%20-%20Ulises%20Antonio%20Samano%20Galvan.pdf?alt=media&token=a5cac99c-1cb9-4e9b-908d-f84eb6089b36',
     },
     {
-      id: 4,
-      description: 'Mi repositorio de GitHub',
-      name: 'Github',
-      path: 'https://github.com/Ulisessg',
+      id: 3,
+      description: 'Perfil de Hacker Rank',
+      name: 'HackerRank',
+      path: 'https://www.hackerrank.com/ukisessg',
     },
   ];
+
   return (
     <footer className="footer" id="contacto">
-      <LinkedInBadge />
-      <div>
-        <Nav paths={footerPaths} backgroundIsLigth={false} />
+      <div className="footer__container">
+        <LazyImage
+          alt="Ulises Antonio Samano Galvan"
+          src="https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2Flogo-192x192.png?alt=media&token=1a4aa1ca-1846-41b3-9005-9c4dc5c4d888"
+          classN="footer--logo"
+        />
+        <h3 className="footer__quote">
+          Si puedes imaginarlo puedes programarlo 💚
+        </h3>
+
+        {/* Social media */}
+
+        <h3 className="footer__quote follow-me">
+          Sigueme en mis redes sociales
+        </h3>
+
+        <div className="footer__social">
+          <a href="https://www.facebook.com/Ulises5G">
+            <LazyImage
+              src="https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2Ffacebook.png?alt=media&token=69833032-271c-4f43-a2d4-c621350b2e54"
+              alt="Logo de Facebook"
+              classN="footer__social--logo"
+            />
+          </a>
+          <a href="https://twitter.com/Ulises5G">
+            <LazyImage
+              src="https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2Ftwitter.png?alt=media&token=539518e5-e18b-4cc4-a41e-498b68caf60b"
+              alt="Logo de twitter"
+              classN="footer__social--logo"
+            />
+          </a>
+          <a href="https://github.com/Ulisessg">
+            <LazyImage
+              src="https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2FGitHub-logo.png?alt=media&token=f7d795cd-d7db-4b35-a2f4-0dca3070dd1b"
+              alt="Logo de github"
+              classN="footer__social--logo"
+            />
+          </a>
+        </div>
+
+        <LinkedInBadge />
+
+        {/* Other links */}
+        <div className="footer__other-links">
+          <Nav paths={footerPaths} backgroundIsLigth={false} col />
+        </div>
+
+        <h3 className="footer__quote coffe-quote">
+          Hecho con amor, café y amor al café 💚
+        </h3>
       </div>
     </footer>
   );
