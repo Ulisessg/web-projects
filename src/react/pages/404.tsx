856ad41429046d8945a0062cfd9febf961fed520
ulisessg <ukisessg@gmail.com>
2021-04-04 18:47:00 -0500

    [REMOVED] REmoved unnecesary use of redux in 404 page

--- src/react/pages/404.tsx
+++ src/react/pages/404.tsx
@@ -1,22 +1,15 @@
 import React, { lazy, Suspense } from 'react';
 import ReactDom from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import Loading from '../atoms/Loading';
-import reducers from '../reducers/error';
 
 import '../../styles/default-styles.styl';
 
 const Error = lazy(() => import('../templates/Error'));
 
-const store = createStore(reducers, {});
-
 ReactDom.render(
   <>
     <Suspense fallback={<Loading />}>
-      <Provider store={store}>
-        <Error />
-      </Provider>
+      <Error />
     </Suspense>
   </>,
   window.document.getElementById('root'),
