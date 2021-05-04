3f0a8172829f16d54c73abd7b15098ba96860b30
ulisessg <ukisessg@gmail.com>
2021-05-04 04:37:31 -0500

    [REFAC] Linkedin Badge component refactorized using Styled Components

--- src/react/molecules/LinkedInBadge.tsx
+++ src/react/molecules/LinkedInBadge.tsx
@@ -1,46 +1,57 @@
 /* eslint-disable react/require-default-props */
 import React from 'react';
 import LazyImage from './Img';
+import {
+  LinkedInBadgeContainer,
+  LinkedInBadgeBackground,
+  LinkedInBadgeName,
+  LinkedInBadgeDescription,
+  LinkedInBadgeGoProfileContiner,
+  ClsStyles,
+} from '../../styles/molecules/LinkedinBadgeStyles';
 
-import '../../styles/molecules/LinkedinBadge.styl';
-// Background image https://static-exp1.licdn.com/sc/h/856xpihrituhwdjrua9z5u5na
+// Background image https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2Flinkedin%20cover.jpeg?alt=media&token=7af29779-027c-4d39-b97e-819707fac55c
 
 // LinkedIn logo https://static-exp1.licdn.com/scds/common/u/images/logos/linkedin/logo_linkedin_93x21_v2.png
 
 function LinkedInBadge({ classN }: { classN?: string }): JSX.Element {
   return (
     <>
-      <section
+      <ClsStyles />
+      <LinkedInBadgeContainer
         className={classN ? `linkedin-badge ${classN}` : 'linkedin-badge'}
       >
-        <div className="linkedin-badge--background">
+        <LinkedInBadgeBackground className="linkedin-badge--background">
           <LazyImage
             src="https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2FSEO%2Fme.jfif?alt=media&token=6f3e9ef4-df67-462c-bba5-0d2bea0e2076"
             alt="Ulises Antonio Sámano Galván"
             classN="linkedin-badge--image"
           />
-        </div>
+        </LinkedInBadgeBackground>
         <a
           className="linkedin-badge--name"
           href="https://mx.linkedin.com/in/ulisessg?trk=profile-badge"
         >
-          <h3>Ulises Antonio Sámano Galván</h3>
+          <strong>
+            <LinkedInBadgeName>Ulises Antonio Sámano Galván</LinkedInBadgeName>
+          </strong>
         </a>
 
-        <p className="linkedin-badge--description">
+        <LinkedInBadgeDescription className="linkedin-badge--description">
           Frontend / Backend / Nerd / Student at Platzi Master.
-        </p>
-        <div className="linkedin-badge--go-profile">
+        </LinkedInBadgeDescription>
+
+        <LinkedInBadgeGoProfileContiner className="linkedin-badge--go-profile">
           <a href="https://mx.linkedin.com/in/ulisessg?trk=profile-badge">
             Ver perfil
           </a>
-        </div>
+        </LinkedInBadgeGoProfileContiner>
         <LazyImage
           classN=""
           alt="Linkedin Logo"
           src="https://static-exp1.licdn.com/scds/common/u/images/logos/linkedin/logo_linkedin_93x21_v2.png"
         />
-      </section>
+      </LinkedInBadgeContainer>
     </>
   );
 }
---
+++ src/styles/molecules/LinkedinBadgeStyles.ts
@@ -0,0 +1,63 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import { backgroundLight } from '../variables';
+
+export const LinkedInBadgeContainer = styled.section`
+  width: 100%;
+  height: 400px;
+  margin-top: 50px;
+  display: flex;
+  display: -webkit-flex;
+  flex-direction: column;
+  background-color: aliceblue;
+  align-items: center;
+  text-align: center;
+  justify-content: space-between;
+  padding-bottom: 15px;
+`;
+
+export const LinkedInBadgeBackground = styled.div`
+  background-image: url(https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2Flinkedin%20cover.jpeg?alt=media&token=7af29779-027c-4d39-b97e-819707fac55c);
+  background-position: center;
+  margin: 0 auto;
+  width: 100%;
+  display: flex;
+  padding: 15px;
+`;
+
+export const LinkedInBadgeName = styled.h3`
+  color: black;
+  &:hover,
+  &:focus {
+    color: ${backgroundLight};
+  }
+  font-size: 1.3rem;
+  padding: 0 10px;
+`;
+
+export const LinkedInBadgeDescription = styled.p`
+  padding: 10px;
+`;
+
+export const LinkedInBadgeGoProfileContiner = styled.div`
+  border: 1px solid ${backgroundLight};
+  padding: 3px;
+`;
+
+export const LinkedInBadgeGoProfileLink = styled.a`
+  padding: 7px;
+  font-size: 1.5rem;
+  color: ${backgroundLight};
+  &:hover,
+  &:focus {
+    color: black;
+  }
+`;
+
+export const ClsStyles = createGlobalStyle`
+  .linkedin-badge--image {
+    border-radius: 100%;
+    width: 100px;
+    margin: 0 auto;
+    border: 4px solid white;
+  }
+`;
