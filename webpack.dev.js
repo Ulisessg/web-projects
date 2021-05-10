4341e7741d261a4f86303b7ddfa0fa0c369d983d
ulisessg <ukisessg@gmail.com>
2021-05-10 13:02:04 -0500

    [DELETE] Workbox disabled in development

--- webpack.dev.js
+++ webpack.dev.js
@@ -62,21 +62,21 @@ module.exports = {
         process.env.FILE.split('.')[0].concat('.html'),
       ),
     }),
-    new WorkboxPlugin.GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
-      clientsClaim: true,
-      skipWaiting: true,
-      runtimeCaching: [
-        { urlPattern: /^https?.*/, handler: 'StaleWhileRevalidate' },
-      ],
-    }),
+    // new WorkboxPlugin.GenerateSW({
+    //   // these options encourage the ServiceWorkers to get in there fast
+    //   // and not allow any straggling "old" SWs to hang around
+    //   clientsClaim: true,
+    //   skipWaiting: true,
+    //   runtimeCaching: [
+    //     { urlPattern: /^https?.*/, handler: 'StaleWhileRevalidate' },
+    //   ],
+    // }),
   ],
   devServer: {
     index: process.env.FILE.split('.')[0].concat('.html'),
     contentBase: join(__dirname, 'src', 'react', 'pages', process.env.FILE),
     watchContentBase: true,
-    port: 8080,
+    port: 2001,
     hot: true,
     open: true,
     watchOptions: {
