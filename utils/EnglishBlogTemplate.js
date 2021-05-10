bc328aa3db2375f965bce447375a7e267e39ff67
ulisessg <ukisessg@gmail.com>
2021-05-10 05:08:54 -0500

    [FIX] Favicon fixed in blogs

--- utils/EnglishBlogTemplate.js
+++ utils/EnglishBlogTemplate.js
@@ -60,7 +60,7 @@ module.exports = function englishBlogTemplate(blog, cssName, jsName) {
 
       <link
         rel="shortcut icon"
-        href="https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2Flogo.png?alt=media&token=6fdc362f-c278-4cfd-a2cb-2c7e95ceab62"
+        href="https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2FLogo-favicon.png?alt=media&token=60a872bd-af09-47dd-a5cc-66e5a5b3ab51"
         type="image/png"
       />
 
