bbf8170fac28896fd55540a880e8b33c5d3b189e
UliseSG <ukisesgs@gmail.com>
2021-05-14 17:23:23 -0500

    [FIX] Gist request limit fixed

--- src/utils/getGists.ts
+++ src/utils/getGists.ts
@@ -1,6 +1,6 @@
 export default function getGists() {
   // @ts-check
-  const request = fetch('https://web-projects-api.vercel.app/api/gist', {
+  const request = fetch('https://web-projects-api.vercel.app/api/gist?limit=15', {
     method: 'GET',
   })
     .then((res) => res.json())
