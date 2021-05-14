258865bc7c4395c7fb989a91dc15b493d0ebabed
UliseSG <ukisesgs@gmail.com>
2021-05-13 19:52:46 -0500

    [ADD] Lazy Image added

--- src/molecules/Img.tsx
+++ src/molecules/Img.tsx
@@ -1,17 +1,18 @@
-import React, { lazy } from 'react';
+import React from 'react';
+import dynamic from 'next/dynamic';
 import { imgProps } from '../interfaces_and_types/globalPropsAndProperties';
 
-// const Element = lazy(() => {
-//   if (navigator !== undefined && 'loading' in HTMLImageElement.prototype) {
-//     return import('../atoms/LazyImage');
-//   }
-//   return import('../atoms/LazyObserverImage');
-// });
+const Element = dynamic(() => {
+  if (navigator !== undefined && 'loading' in HTMLImageElement.prototype) {
+    return import('../atoms/LazyImage');
+  }
+  return import('../atoms/LazyObserverImage');
+}, { ssr: false });
 
 function Img({ src, alt, classN }: imgProps): JSX.Element {
   return (
     <>
-      <img className={classN} src={src} alt={alt} />
+      <Element className={classN} src={src} alt={alt} />
     </>
   );
 }
