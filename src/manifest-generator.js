a0d2e7552e25bd921600b9b86316c111568a5726
UliseSG <ukisesgs@gmail.com>
2021-05-14 07:23:04 -0500

    [ADD] Manifest generator added

---
+++ src/manifest-generator.js
@@ -0,0 +1,6 @@
+const { copyFileSync } = require('fs');
+const { join } = require('path');
+
+copyFileSync(join(__dirname, 'pwa', 'manifest.json'), join(__dirname, 'out', 'manifest.json'));
+
+process.stdout.write('Maifest generated');
