4fb9b708c8a65e9fcbdfd3b7ff4b432488414592
UliseSG <ukisesgs@gmail.com>
2021-05-15 07:04:46 -0500

    [ADD] Robots.txt generator added

---
+++ src/postbuild/robots-txt-generator.js
@@ -0,0 +1,15 @@
+const { writeFileSync } = require('fs');
+const { join } = require('path');
+
+writeFileSync(
+  join(__dirname, '..', 'out', 'robots.txt'),
+  `User-Agent: *
+Allow: /
+Allow: /en
+Allow: /gists
+
+Sitemap: https://ulisessg.com/sitemap.xml
+  `,
+);
+
+process.stdout.write('robots.txt generated');
