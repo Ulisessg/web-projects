f496e0bf6dc89484c309f8c82d836fff49e3c6ea
UliseSG <ukisesgs@gmail.com>
2021-06-27 13:16:09 -0500

    [FIX] Share Media Styles fixed

--- src/organisms/ShareMedia.tsx
+++ src/organisms/ShareMedia.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Script from 'next/script';
 import dynamic from 'next/dynamic';
+import ShareMediaStyles from '../styles/organisms/ShareMediaStyles';
 
 const Heart = dynamic(() => import('../atoms/Heart'), { ssr: false });
 
@@ -25,7 +26,7 @@ function ShareMedia({
       <Script async defer crossOrigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v11.0" nonce="PBBx30mF" />
       {/* Close Facebook share script */}
 
-      <section style={{ marginTop: '15px', display: 'flex', alignItems: 'center' }}>
+      <ShareMediaStyles>
         <div>
           <Heart name={documentNameForLike} url={addLikePath} />
         </div>
@@ -34,7 +35,7 @@ function ShareMedia({
         <div>
           <a target="_blank" href={`https://www.linkedin.com/shareArticle?mini=true&url=https://ulisessg.com${path}&title=${title}&summary=${description}`} rel="noreferrer"><img alt="Compartir en LinkedIn Logo" src="https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2Fblogs%2Fshare%20social%20media%2FLinkedIn%20share%20logo.png?alt=media&token=0be89f71-48aa-46bd-9c36-82c0e6e355f3" width="90px" height="auto" /></a>
         </div>
-      </section>
+      </ShareMediaStyles>
     </>
   );
 }
---
+++ src/styles/organisms/ShareMediaStyles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ShareMediaStyles = styled.section`
+  margin-top: 15px;
+  display: flex;
+  display: -webkit-flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export default ShareMediaStyles;
