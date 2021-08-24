46ffce3842a8969ed659463ebc301dd523acee06
UliseSG <ukisesgs@gmail.com>
2021-08-24 00:38:15 -0500

    [ADD] Other blogs added

---
+++ src/states/blogsState.ts
@@ -0,0 +1,50 @@
+import SectionProps from '../interfaces_and_types/organisms/SectionProps';
+
+const otherPosts: Array<SectionProps> = [
+  {
+    description: 'El pasado simple nos sirve para expresar acciones que ya sucedieron, que ya finalizaron,por lo que debemos cambiar la forma en que escribimos los verbos, en el pasado simple utilizamos los verbos regulares e irregulares.',
+    id: 'El pasado simple',
+    image: {
+      src: 'https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2Fother%20blogs%2FPasado%20simple.png?alt=media&token=36c4e22f-f748-45e8-9bed-35d8bec609a0',
+      alt: 'Pasado simple en inglés',
+    },
+    name: 'Pasado simple en inglés',
+    title: 'El pasado simple en inglés',
+    path: 'https://platzi.com/tutoriales/1370-ingles-gramatica/5900-el-pasado-simple-en-ingles/',
+  },
+  {
+    description: 'El método Getting Things Done pertenece a David Allen y se basa en utilizar listas de tareas para mantener la mente tranquila y así poder concentrarnos en las tareas en lugar de pensar ¿Qué tengo que hacer después?',
+    id: 'Cómo utilizar el método Getting Things Done de forma efectiva',
+    image: {
+      src: 'https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2Fother%20blogs%2FGTD.png?alt=media&token=0878ffb7-162e-475d-878a-d1f5b0292976',
+      alt: 'Cómo utilizar el método Getting Things Done de forma efectiva',
+    },
+    name: 'Cómo utilizar el método Getting Things Done de forma efectiva',
+    title: 'Cómo utilizar el método Getting Things Done de forma efectiva',
+    path: 'https://platzi.com/tutoriales/1749-gestion-tiempo/5778-como-utilizar-el-metodo-getting-things-done-de-forma-efectiva/',
+  },
+  {
+    description: 'Node es un entorno de ejecución de JavaScript que corre sobre el motor v8 de Chrome, en palabras mortales Node nos permite ejecutar nuestros programas de JavaScript sin necesidad de un navegador',
+    id: 'Lo básico de Node',
+    image: {
+      src: 'https://media-exp1.licdn.com/dms/image/C5612AQHA6gT_xne0YQ/article-cover_image-shrink_720_1280/0/1606547236941?e=1635379200&v=beta&t=ryZltqoboKxxFRch-RHa66hFF5FxtBU-Z9eYacmWWAM',
+      alt: 'Lo básico de Node',
+    },
+    name: 'Lo básico de Node',
+    title: 'Lo básico de Node',
+    path: 'https://www.linkedin.com/pulse/lo-b%C3%A1sico-de-node-ulises-antonio-s%C3%A1mano-galv%C3%A1n/',
+  },
+  {
+    description: 'Las bases de datos utilizan un lenguaje especializado para consultar información, las bases de datos relacionales utilizan SQL(Structured Query Language), en el caso de MongoDB se utiliza MQL (MongoDB Query Language)',
+    id: 'Consultas básicas de MongoDB',
+    image: {
+      src: 'https://media-exp1.licdn.com/dms/image/C5612AQEWFFmaai4Bvw/article-cover_image-shrink_720_1280/0/1609054864646?e=1635379200&v=beta&t=oz7pQ385vPRBI9YWnU2llEB5jdJXX2KsJvKb2hKYUfE',
+      alt: 'Consultas básicas de MongoDB',
+    },
+    name: 'Consultas básicas de MongoDB',
+    title: 'Consultas básicas de MongoDB',
+    path: 'https://www.linkedin.com/pulse/consultas-b%C3%A1sicas-de-mongodb-ulises-antonio-s%C3%A1mano-galv%C3%A1n/',
+  },
+];
+
+export default otherPosts;
--- src/templates/Blog.tsx
+++ src/templates/Blog.tsx
@@ -3,6 +3,7 @@ import SectionContainer from '../styles/molecules/SectionContainer';
 import SectionNoModal from '../organisms/SectionNoModal';
 import SectionProps from '../interfaces_and_types/organisms/SectionProps';
 import H2 from '../atoms/H2';
+import otherBlogs from '../states/blogsState';
 
 function Blog({ blogEntries, isPageOrSection }:
   { blogEntries: Array<SectionProps>; isPageOrSection: 'page' | 'section'; }): JSX.Element {
@@ -23,6 +24,28 @@ function Blog({ blogEntries, isPageOrSection }:
               <SectionContainer>
 
                 {blogEntries.map((blog) => (
+                  <SectionNoModal
+                    id={blog.name}
+                    image={blog.image}
+                    name={blog.name}
+                    title={blog.title}
+                    key={blog.name}
+                    path={blog.path}
+                    description={blog.description}
+                  />
+                ))}
+              </SectionContainer>
+
+            </section>
+          </div>
+
+          <H2 text="Otros posts" />
+          <div id="posts">
+            <section className="sections">
+
+              <SectionContainer>
+
+                {otherBlogs.map((blog) => (
                   <SectionNoModal
                     id={blog.name}
                     image={blog.image}
