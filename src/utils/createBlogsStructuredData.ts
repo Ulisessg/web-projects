3362a08aa546994b6b24e0d79f3e18cfb1856eab
UliseSG <ukisesgs@gmail.com>
2021-10-18 18:00:22 -0500

    [FIX] Blog structured data fixed

--- src/utils/createBlogsStructuredData.ts
+++ src/utils/createBlogsStructuredData.ts
@@ -1,4 +1,4 @@
-import { BlogEntryRaw } from "../interfaces_and_types/globalPropsAndProperties";
+import { BlogEntryRaw } from '../interfaces_and_types/globalPropsAndProperties';
 
 interface createStructuredDataReturn {
   date: string;
@@ -8,40 +8,47 @@ interface createStructuredDataReturn {
 export default function createBlogsStructuredData(
   info: BlogEntryRaw
 ): createStructuredDataReturn {
-  const dateSplitted: Array<string> = info.publicationDate?.split("-") as any;
+  const dateSplitted: Array<string> = info.publicationDate?.split('-') as any;
   const blogDate: Date = new Date(
     parseInt(dateSplitted[0], 10),
     parseInt(dateSplitted[1], 10),
     parseInt(dateSplitted[2], 10)
   );
-  const date = blogDate.toLocaleDateString("es-MX", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
+  const date = blogDate.toLocaleDateString('es-MX', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
   });
-  const dateFormated = blogDate.toJSON().slice(0, 10);
+  const dateFormated = blogDate.toISOString();
   const structuredData = {
-    "@context": "https://schema.org",
-    "@type": "Article",
+    '@context': 'https://schema.org',
+    '@type': 'Article',
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': `https://ulisessg.com/${info.name}`,
+    },
     headline: `${info.title} | UlisesSG`,
     datePublished: dateFormated,
-    image: info.seoCardUrl,
-    articleBody: info.content.replace(/<[^>]*>?/gm, ""),
+    dateModified: info.lastUpdate
+      ? new Date(info.lastUpdate).toISOString()
+      : dateFormated,
+    image: [info.seoCardUrl],
+    articleBody: info.content.replace(/<[^>]*>?/gm, ''),
     publisher: {
-      "@type": "Organization",
+      '@type': 'Organization',
       logo: {
-        "@type": "ImageObject",
-        url: "https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2Flogo-192x192.png?alt=media&token=1a4aa1ca-1846-41b3-9005-9c4dc5c4d888",
+        '@type': 'ImageObject',
+        url: 'https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/images%2Fv2%2Flogo-192x192.png?alt=media&token=1a4aa1ca-1846-41b3-9005-9c4dc5c4d888',
       },
-      name: "Ulises Antonio Samano Galvan",
+      name: 'Ulises Antonio Samano Galvan',
     },
-    about: { "@type": "CreativeWork" },
+    about: { '@type': 'CreativeWork' },
     author: {
-      "@type": "Person",
-      name: "Ulises Antonio Samano Galvan",
-      jobTitle: "Web UI Developer",
-      url: "https://ulisessg.com",
+      '@type': 'Person',
+      name: 'Ulises Antonio Samano Galvan',
+      jobTitle: 'Web UI Developer',
+      url: 'https://ulisessg.com',
     },
   };
   const structuredDataJson = JSON.stringify(structuredData);
