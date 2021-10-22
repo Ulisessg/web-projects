1685afaf1429b231fc277a85f67b63ab45e2c5a0
UliseSG <ukisesgs@gmail.com>
2021-10-22 17:40:27 -0500

    [ACCESIBILITY] Section No Modal, tab index removed in image

--- src/organisms/SectionNoModal.tsx
+++ src/organisms/SectionNoModal.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
-import Link from "../atoms/Link";
+import React from 'react';
+import Link from '../atoms/Link';
 import {
   SectionContainer,
   ImageContainer,
   Title,
   Description,
-} from "../styles/organisms/SectionStyles";
-import { SectionProps } from "../types/props";
+} from '../styles/organisms/SectionStyles';
+import { SectionProps } from '../types/props';
 function SectionNoModal({
   image,
   name,
@@ -20,30 +20,32 @@ function SectionNoModal({
 }: SectionProps): JSX.Element {
   return (
     <>
-      <SectionContainer className="section--container" key={id}>
-        <ImageContainer className="section-img-container">
-          <img
-            loading="lazy"
-            className="section--img"
-            src={image.src}
-            alt={image.alt}
-          />
+      <SectionContainer className='section--container' key={id}>
+        <ImageContainer className='section-img-container'>
+          <a tabIndex={-1} href={`${path || name}`}>
+            <img
+              loading='lazy'
+              className='section--img'
+              src={image.src}
+              alt={image.alt}
+            />
+          </a>
         </ImageContainer>
 
-        <Title className="section--title">{title}</Title>
+        <Title className='section--title'>{title}</Title>
 
-        <Description className="section--description">
+        <Description className='section--description'>
           {description}
         </Description>
 
         <Link
           href={path || name}
           text={`Ver más sobre ${title}`}
-          background="backgroundLight"
-          bgh="backgroundLight2"
-          cn="some"
-          ct="textDark"
-          cth="textDark2"
+          background='backgroundLight'
+          bgh='backgroundLight2'
+          cn='some'
+          ct='textDark'
+          cth='textDark2'
           linkSize={linkSize}
           ariaLabel={loadingLabel}
         />
